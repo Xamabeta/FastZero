eae7abadbb1dcc2adb27399984b2a28f0291ce45
Xamabeta <lucianomf306@gmail.com>
2024-10-21 23:31:14 -0300

    novo commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from sqlalchemy.pool import StaticPool
 from fast_api.app import app
 from fast_api.database import get_session
 from fast_api.models import User, table_registry
+from fast_api.security import get_password_hash
 
 
 @pytest.fixture()
@@ -38,13 +39,25 @@ def session():
 
 @pytest.fixture()
 def user(session):
+    password = 'testarmando'
     user = User(
         username='armandoteste',
         email='armandotest@teste.com',
-        password='testarmando',
+        password=get_password_hash(password),
     )
     session.add(user)
     session.commit()
     session.refresh(user)
 
+    user.clean_password = 'testarmando'
+
     return user
+
+
+@pytest.fixture()
+def token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    return response.json()['access_token']
